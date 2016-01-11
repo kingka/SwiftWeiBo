d00037b0bf91ea28328a51df2d5fb183fde69f29
ImanolZhang <jingkang_zhang@vandagroup.com>
2016-01-11 21:43:59 +0800

    initial project

---
+++ SwiftWeiBo/SwiftWeiBo/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  SwiftWeiBo
+//
+//  Created by Imanol on 1/11/16.
+//  Copyright © 2016 imanol. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        let vc = ViewController()
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
