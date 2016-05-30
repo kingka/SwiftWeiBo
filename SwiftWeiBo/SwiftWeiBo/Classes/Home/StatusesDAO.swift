e2480d1e4babdc3047810a06a43fec0b3b1eec90
Imanol <jk199054@163.com>
2016-05-30 11:17:06 +0800

    集成FMDB

---
+++ SwiftWeiBo/SwiftWeiBo/Classes/Home/StatusesDAO.swift
@@ -0,0 +1,18 @@
+//
+//  StatusesDAO.swift
+//  SwiftWeiBo
+//
+//  Created by Imanol on 16/5/30.
+//  Copyright © 2016年 imanol. All rights reserved.
+//
+
+import UIKit
+
+class StatusesDAO: NSObject {
+    
+    func loadStatues(){
+        
+        let dbQueue = FMDatabaseQueue(path: "")
+    }
+
+}
---
+++ SwiftWeiBo/SwiftWeiBo/FMDB-Bridge.h
@@ -0,0 +1,9 @@
+//
+//  FMDB-Bridge.h
+//  SwiftWeiBo
+//
+//  Created by Imanol on 16/5/30.
+//  Copyright © 2016年 imanol. All rights reserved.
+//
+
+#import "FMDB.h"
